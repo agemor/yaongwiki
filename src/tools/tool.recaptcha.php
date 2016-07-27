c6f51019232a90699713102894db560ee9136706
HyunJun Kim <HyunJun Kim>
2016-07-27 21:41:22 +0900

    Add recaptcha fallback for fopen unavailability

--- src/tools/tool.recaptcha.php
+++ src/tools/tool.recaptcha.php
@@ -7,14 +7,22 @@
  * @date 2016. 02. 01
  */
 
-require_once 'libs/recaptcha/autoload.php';
-
-const RECAPTCHA_PRIVATE_KEY = '6LewehYTAAAAAFZJJRnqErvewMQ_dY8oCxW7g2n6';
-const RECAPTCHA_PUBLIC_KEY = '6LewehYTAAAAAMoePWzUQITyPGXSXLppuarmvjrm';
+const RECAPTCHA_PUBLIC_KEY = '6LfLFyYTAAAAAE0pczd8EZIuKpY2ylZytYfUD_oX';
+const RECAPTCHA_PRIVATE_KEY = '6LfLFyYTAAAAABI18Db9cDTmvJZZDcq3jgmGuQ5A';
 
 function getReCaptcha() {
-    $recaptcha = new \ReCaptcha\ReCaptcha(RECAPTCHA_PRIVATE_KEY);
-    $response  = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
-    return $response->isSuccess();
+
+	$secret = RECAPTCHA_PRIVATE_KEY;
+	$response = $_POST["g-recaptcha-response"];
+	$remoteip = $_SERVER['REMOTE_ADDR'];
+
+	$verifyResponse = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$secret}&response={$response}&remoteip={$remoteip}&");
+
+	if ($verifyResponse == false)
+		return true;
+
+	$responseData = json_decode($verifyResponse);
+
+	return $responseData->success;
 }
 ?>
