1e9dcbb4ed01e36cff623158fc08516f2495fd9c
HyunJun Kim <hyunjun.leo.kim@gmail.com>
2017-08-26 21:19:39 +0900

    Remove .htaccess for nginx support

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# Yaong Wiki
+# YaongWiki
 
 > Lightweight [WYSIWYG](https://en.wikipedia.org/wiki/WYSIWYG) wiki engine for general use
 
 ## Installation for developers (Using [Docker](https://www.docker.com/))
-For contributors and testers, Yaong Wiki have ready-to-launch docker configuration. Access through `localhost:8001` is available after starting the docker container.
+For contributors and testers, YaongWiki have ready-to-launch docker configuration. Access through `localhost:8001` is available after starting the docker container.
 
 ```
 $ docker-compose up
 ```
 
 ## License
-By the terms of [GPL-2.0](#) license, Yaong Wiki can be used by anyone. 
+By the terms of [GPL-2.0](#) license, YaongWiki can be used by anyone. 
--- src/common.php
+++ src/common.php
@@ -2,17 +2,17 @@
 /**
  * YaongWiki Engine
  *
- * @version 1.1
+ * @version 1.2
  * @author HyunJun Kim
- * @date 2016. 01. 31
+ * @date 2017. 08. 26
  */
 
-const DB_HOST = '{DB_HOST}';
-const DB_USER = '{DB_USER}';
-const DB_NAME = '{DB_NAME}';
-const DB_PASSWORD = '{DB_PASSWORD}';
+const DB_HOST = "{DB_HOST}";
+const DB_USER = "{DB_USER}";
+const DB_NAME = "{DB_NAME}";
+const DB_PASSWORD = "{DB_PASSWORD}";
 
-const FILE_MAXIMUM_SIZE = 10485760; // 10MB
+const FILE_MAXIMUM_SIZE = 10 * 1024 * 1024; // 10MB
 const FILE_DIRECTORY = "uploads";
 const FILE_AVALIABLE_EXTENSIONS = array("jpg", "jpeg", "png", "gif", "svg", "tiff", "bmp");
 
@@ -20,74 +20,74 @@ const PERMISSION_NO_FILTERING = 3;
 const PERMISSION_CHANGE_TITLE = 1;
 const PERMISSION_DELETE_ARTICLE = 1;
 
-const TITLE_AFFIX = ' - 연세위키';
+const TITLE_AFFIX = " - YaongWiki";
 
-const HREF_MAIN = './';
-const HREF_SIGNIN = './signin';
-const HREF_SEARCH = './keywords';
-const HREF_SIGNUP = './signup';
-const HREF_SIGNOUT = './signout';
-const HREF_RESET = './reset';
-const HREF_WRITE = './edit';
-const HREF_READ = './pages';
-const HREF_DASHBOARD = './dashboard';
-const HREF_PROFILE = './users';
-const HREF_CREATE = './create';
-const HREF_REVISIONS = './revisions';
-const HREF_REVISION = './revision';
-const HREF_404 = './404';
-const HREF_SUGGEST = './suggest';
-const HREF_RECENT = './recent';
+const HREF_MAIN = "./";
+const HREF_SIGNIN = "./page.signin.php";
+const HREF_SEARCH = "./page.search.php";
+const HREF_SIGNUP = "./page.signup.php";
+const HREF_SIGNOUT = "./page.signout.php";
+const HREF_RESET = "./page.reset.php";
+const HREF_WRITE = "./page.write.php";
+const HREF_READ = "./page.read.php";
+const HREF_DASHBOARD = "./page.dashboard.php";
+const HREF_PROFILE = "./page.profile.php";
+const HREF_CREATE = "./page.create.php";
+const HREF_REVISIONS = "./page.revisions.php";
+const HREF_REVISION = "./page.revision.php";
+const HREF_404 = "./page.404.php";
+const HREF_SUGGEST = "./page.suggest.php";
+const HREF_RECENT = "./page.recent.php";
 
-const LOG_DELETE_ACCOUNT = 'delete-account';
-const LOG_STUDENT_AUTH = 'auth';
-const LOG_CHANGE_EMAIL = 'change-email';
-const LOG_CHANGE_PASSWORD = 'change-password';
-const LOG_WRITE = 'write';
-const LOG_CREATE = 'create';
-const LOG_UPDATE_USER_INFO = 'update-user-info';
-const LOG_RESET = 'reset';
-const LOG_SIGNIN = 'signin';
-const LOG_SIGNUP = 'signup';
+const LOG_DELETE_ACCOUNT = "delete-account";
+const LOG_STUDENT_AUTH = "auth";
+const LOG_CHANGE_EMAIL = "change-email";
+const LOG_CHANGE_PASSWORD = "change-password";
+const LOG_WRITE = "write";
+const LOG_CREATE = "create";
+const LOG_UPDATE_USER_INFO = "update-user-info";
+const LOG_RESET = "reset";
+const LOG_SIGNIN = "signin";
+const LOG_SIGNUP = "signup";
 
 function permissionInfo($permission) {
     switch ($permission) {
         case 0:
             return array(
-                'description' => '독자',
-                'color' => 'info'
+                "description" => "Reader",
+                "color" => "info"
             );
         case 1:
             return array(
-                'description' => '편집자',
-                'color' => 'warning'
+                "description" => "Editor",
+                "color" => "warning"
             );
         case 2:
             return array(
-                'description' => '중재자',
-                'color' => 'success'
+                "description" => "Moderator",
+                "color" => "success"
             );
         case 3:
             return array(
-                'description' => '관리자',
-                'color' => 'danger'
+                "description" => "Administrator",
+                "color" => "danger"
             );
         default:
             return array(
-                'description' => '개발자',
-                'color' => 'primary'
+                "description" => "System Administrator",
+                "color" => "primary"
             );
             
     }
 }
 
 function navigateTo($link = HREF_MAIN) {
-    header('Location: ' . $link);
+    header("Location: " . $link);
     exit();
 }
 
 function passwordHash($password) {
-    return hash('sha512', $password . 'yw');
+    return hash("sha512", $password . "yw");
 }
 
 ?>
