817d2c3f58b28e86014bbf64d543cf0b5d460d21
HyunJun Kim <hyunjun.leo.kim@gmail.com>
2017-09-02 17:31:28 +0900

    Refactor recent changes module

--- src/core/core.page.recent.php
+++ src/core/core.page.recent.php
@@ -2,123 +2,32 @@
 /**
  * YaongWiki Engine
  *
- * @version 1.1
+ * @version 1.2
  * @author HyunJun Kim
- * @date 2016. 01. 31
+ * @date 2017. 09. 12
  */
 
-require_once 'common.php';
-require_once 'common.db.php';
+require_once "core.php";
+require_once "core.db.php";
+require_once "core.session.php";
 
 const MAX_RECENT_CHANGED = 30;
 
-function main() {
+function process() {
 
-	global $session;
-    global $db_connect_info;
+    global $db;
 
-     $db = new YwDatabase($db_connect_info);
+    $response = $db->custom("SELECT * FROM " . DB_REVISION_TABLE . " WHERE `id` IN (SELECT MAX(`id`) FROM " . DB_REVISION_TABLE . " GROUP BY `article_id`) ORDER BY `id` DESC LIMIT " . MAX_RECENT_CHANGED . ";");
     
-    // 데이터베이스 연결을 체크한다.
-    if (!$db->connect())
+    if (!$response) {
         return array(
-            'result'=>false,
-            'message'=>'서버와의 연결에 실패했습니다'
+            "result" => false,
+            "message" => STRINGS["EPRC0"]
         );
-    
-    if (!$db->query("SELECT * FROM " . REVISION_TABLE . " WHERE `id` IN (SELECT MAX(`id`) FROM " . REVISION_TABLE . " GROUP BY `article_id`) ORDER BY `id` DESC LIMIT " . MAX_RECENT_CHANGED . ";"))
-        return array(
-            'result'=>false,
-            'message'=>'서버와의 연결에 실패했습니다'
-        );
-    
-    if ($db->total_results() < 1)
-        return array(
-            'result'=>false,
-            'message'=>'서버와의 연결에 실패했습니다'
-        );
-    
-    $result_array = array();
-    
-    while ($result = $db->get_result()) {
-        array_push($result_array, $result);
     }
     
     return array(
-            'result'=>true,
-            'recent'=>$result_array,
-            'message'=>''
-        );
+        "result" => true,
+        "recent" => $response
+    );
 }
-
-$page_response = main();
-$page_title = "최근 변경된 지식";
-$page_location = HREF_RECENT;
-
-include 'frame.header.php';?>
-
-<div class="container">
-  <h1><a style="text-decoration: none;" href="#">최근 변경된 지식</a></h1>
-  <br/>
-  <hr/>
-  <blockquote>
-    <p>최근 업데이트된 30개의 지식 목록입니다.</p>
-    <footer>다들 열심히 사는군...  <cite> - 중도에서 공부하는 C군</cite></footer>
-  </blockquote>
-
-  <table class="table table-hover">
-    <thead>
-      <tr>
-        <th class="text-center" style="width: 2%">#</th>
-        <th class="text-center" style="width: 25%">지식</th>
-        <th class="text-center" style="width: 15%">편집자</th>
-        <th class="text-center" style="width: 10%">변동</th>
-        <th class="text-center" style="width: 10%">비교</th>
-        <th class="text-center" style="width: 20%">편집 시간</th>
-      </tr>
-    </thead>
-    <tbody class="text-center">
-      <?php
-        if (count($page_response['recent']) < 1) {
-            echo '<tr><td></td><td></td><td><em>아직 기여 기록이 없습니다.</em></td><td></td><td></td><td></td></tr>';
-        } 
-        
-        $next_id = 0; // 무설정시 원본글과 대조
-
-        foreach ($page_response['recent']  as $result) {
-            echo '<tr>';
-            echo '<td id="c"><a href="'.HREF_REVISION.'?i='.$result["id"].'&j='.$next_id.'">'.$result["id"].'</a></td>';
-            echo '<td id="c"><a href="'.HREF_READ.'/'.$result["article_id"].'">'.$result["article_title"].'</a></td>';
-            
-
-
-            if (strlen($result["comment"]) > 0) {
-                echo '<td id="c"><a href="'.HREF_PROFILE.'/'.$result["user_name"].'"">'.$result["user_name"].'</a><br>('.$result["comment"].')</td>';
-            } else {
-                echo '<td id="c"><a href="'.HREF_PROFILE.'/'.$result["user_name"].'"">'.$result["user_name"].'</a></td>';
-            }
-        
-            $fluctuation = intval($result["fluctuation"]);    
-            if($fluctuation > 0) {
-                echo '<td id="c"><span style="color:green"><span class="glyphicon glyphicon-arrow-up" aria-hidden="true" ></span> '.$fluctuation.'</span>';
-            } else if ($fluctuation == 0) {
-                echo '<td id="c"><span style="color:grey"><span class="glyphicon glyphicon-minus" aria-hidden="true" ></span> 0</span>';
-            } else {
-                echo '<td id="c"><span style="color:red"><span class="glyphicon glyphicon-arrow-down" aria-hidden="true" ></span> '.abs($fluctuation).'</span>';
-            }
-            echo '</td>';
-            echo '<td id="c"> <a href="'.HREF_REVISION.'?i='.$result["id"].'&j='.$next_id.'" class="btn btn-xs btn-default" type="button">비교</a>';
-           
-            echo '</td>';
-            echo '<td id="c">'.$result["timestamp"].'</td>';
-            echo '</tr>';
-
-            $next_id = $result["id"];
-        }?>
-    </tbody>
-  </table>
-
-
-</div>
-
-<?php include 'frame.footer.php';?>
--- src/core/core.page.reset.php
+++ src/core/core.page.reset.php
@@ -2,9 +2,9 @@
 /**
  * YaongWiki Engine
  *
- * @version 1.1
+ * @version 1.2
  * @author HyunJun Kim
- * @date 2016. 01. 31
+ * @date 2017. 09. 12
  */
 
 require_once 'common.php';
@@ -96,53 +96,3 @@ function main() {
         'message'=>'이메일로 아이디와 새로운 비밀번호를 전송했습니다'
     );
 }
-
-$page_response = main();
-$page_title    = "계정 찾기";
-$page_location = HREF_RESET;
-
-include 'frame.header.php';
-?>
-
-<div class="container">
-  <h1><a href="#" style="text-decoration: none;">계정 찾기</a></h1><br/>
-  <hr/>
-  <div class="row">
-    <div class="col-md-6">
-      <blockquote>
-        <p>계정 생성 시 입력했던 이메일 주소를 입력해 주세요.</p>
-        <footer>어쩌지... 생각이 안 나. <cite> - 첫 중간고사를 보는 새내기 L양</cite></footer>
-      </blockquote>
-    </div>
-    <div class="col-md-6">
-      <form style="width:auto; margin:auto;" action="<?php echo HREF_RESET;?>" method="post">
-        <div class="well">
-          <?php
-            if(!$page_response['result']) {
-            	echo '<div class="alert alert-danger alert-dismissible" role="alert">';
-            	echo '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
-            	echo $page_response['message'];
-            	echo '</div>';
-            } else if (!empty($page_response['message'])) {
-            	echo '<div class="alert alert-success alert-dismissible" role="alert">';
-            	echo '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
-            	echo $page_response['message'];
-            	echo '</div>';
-            }?>
-          <div style="margin-bottom: 10px" class="input-group">
-            <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
-            <input type="text" name="user-email" class="form-control" placeholder="이메일 주소" value="<?php echo $http_user_email;?>" required <?php if(!$error) { echo "autofocus"; }?>>
-          </div>
-          <script src='https://www.google.com/recaptcha/api.js'></script>
-          <div class="g-recaptcha" style="margin-top: 10px; margin-bottom: 20px; float: right;" data-sitekey="<?php echo RECAPTCHA_PUBLIC_KEY;?>" ></div>
-          <input type="hidden" name="redirect" value="<?php echo $redirect;?>">
-          <button class="btn  btn-primary btn-block" type="submit">계정 정보 받기</button>
-          <a class="btn btn-default btn-block" href="<?php echo HREF_SIGNIN;?>" role="button">취소하기</a>
-        </div>
-      </form>
-    </div>
-  </div>
-  <br/><br/>
-</div>
-
-<?php include 'frame.footer.php';?>
