f7c8483a70c3898993f3355870252be724b9a66a
HyunJun Kim <hyunjun.leo.kim@gmail.com>
2017-09-21 15:59:47 +0900

    Complete profile page in default theme

--- src/themes/default/page.profile.php
+++ src/themes/default/page.profile.php
@@ -2,217 +2,152 @@
 /**
  * YaongWiki Engine
  *
- * @version 1.1
+ * @version 1.2
  * @author HyunJun Kim
- * @date 2016. 01. 31
+ * @date 2017. 09. 20
  */
 
-require_once 'common.php';
-require_once 'common.db.php';
-require_once 'common.session.php';
+require_once YAONGWIKI_CORE . "/page.profile.processor.php";
 
-const MAX_REVISIONS = 20;
+$page = process();
 
-function main() {
-    
-    global $session;
-    global $db_connect_info;
-
-    $http_user_name        = trim(!empty($_GET['name']) ? $_GET['name'] : $_POST['user-name']);
-    $http_user_info        = strip_tags($_POST['user-info']);
-    $http_user_commit_page = intval(!empty($_GET['p']) ? $_GET['p'] : '0');
-
-    if (empty($http_user_name))
-        return array(
-            'result'=>false,
-            'message'=>'존재하지 않는 유저입니다'
-        );
-    
-    if (strlen($http_user_name) < 3)
-        return array(
-            'result'=>false,
-            'message'=>'존재하지 않는 유저입니다'
-        );
-    
-    $db = new YwDatabase($db_connect_info);
-    
-    // 데이터베이스 연결을 체크합니다.
-    if (!$db->connect())
-        return array(
-            'result'=>false,
-            'message'=>'서버와의 연결에 실패했습니다'
-        );
-    
-    // 아이디가 유효한지 확인합니다.
-    if (!$db->query("SELECT * FROM " . USER_TABLE . " WHERE `name`='" . $db->purify($http_user_name) . "';"))
-        return array(
-            'result'=>false,
-            'message'=>'유저 정보를 불러오는데 실패했습니다'
-        );
-    
-    if ($db->total_results() < 1)
-        return array(
-            'result'=>false,
-            'message'=>'존재하지 않는 아이디입니다'
-        );
-    
-    $user = $db->get_result();
-    
-    if (!$db->query("SELECT * FROM " . REVISION_TABLE . " WHERE `user_name`='" . $db->purify($http_user_name) . "' ORDER BY `timestamp` DESC LIMIT " . ($http_user_commit_page * MAX_REVISIONS) . "," . MAX_REVISIONS . ";"))
-        return array(
-            'result'=>false,
-            'user'=>$user,
-            'message'=>'유저 기여 정보를 불러오는데 실패했습니다'
-        );
-    $user['contributions'] = array();    
-    while ($result = $db->get_result())
-        array_push($user['contributions'], $result);
-
-    // 만약 자기소개 업데이트를 요청했다면,
-    if (!empty($http_user_info) && $session->id == $user['id'] && strcmp($user['info'], $http_user_info) != 0) {
-        if (!$db->query("UPDATE " . USER_TABLE . " SET `info`='" . $db->purify($http_user_info) . "' WHERE `name`='" . $db->purify($http_user_name) . "';"))
-            return array(
-                'result'=>false,
-                'user'=>$user,
-                'page'=>$http_user_commit_page,
-                'message'=>'유저 정보를 업데이트하는데 실패했습니다'
-            );
-        $user['info'] = $http_user_info;
-        $db->log($http_user_name, LOG_UPDATE_USER_INFO, $http_user_info);
-    }
-    
-    return array(
-        'result'=>true,
-        'user'=>$user,
-        'page'=>$http_user_commit_page,
-        'message'=>''
-    );
+if (isset($page["redirect"]) && $page["redirect"] == true) {
+    $redirect->redirect();  
 }
 
-$page_response = main();
-$page_title    = $page_response['user']['name'] . '의 프로필';
-$page_location = HREF_PROFILE . '/' . $page_response['user']['name'];
+$page["title"] = "Profile of " . $page["user"]["name"];
 
-
-if (!$page_response['result'])
-   navigateTo(HREF_404);
-
-include 'frame.header.php';
+require_once __DIR__ . "/frame.header.php";
 ?>
 
-<style type="text/css">
-  #c {
-  display: table-cell;
-  vertical-align: middle;
-  }
-</style>
 <div class="container">
-<h1><a href="#" style="text-decoration: none;">
-<?php
-    $permission_info = permissionInfo(intval($page_response['user']['permission']));
-    echo $page_response['user']['name'];
-    echo '  <a role="button" class="btn btn-xs btn-'
-       .$permission_info['color'].'">'.$permission_info['description'].'</a>';?>
-</a></h1><br/><hr/>
-<?php
-if (!$page_response['result']) {
-  echo '<div class="alert alert-dismissible alert-danger" role="alert">'.$page_response['message'];
-  echo '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
-  echo '</div>';
-}?>
+  <div class="title my-4">
+    <h2>
+    <?php echo($page["user"]["name"]);?>
+    <small class="text-muted"> (Level: <em><?php echo($page["user"]["permission"]);?>)</em></small>
+    <h2>
+  </div>
+  <?php if (isset($page["result"]) && $page["result"] !== true) { ?>
+  <div class="alert alert-danger" role="alert">
+    <?php echo($page["message"]);?>
+  </div>
+  <?php } ?>
 
-<div class="row">
-  <div class="col-md-12">
-    <blockquote>
-      <p>
-      <?php
-      if (empty($page_response['user']['info'])) {
-          echo "<em>자기소개 정보가 없습니다.</em>";
-      } else {
-          echo $page_response['user']['info'];
-      }
-      if ($session->id == $page_response['user']['id']) {?>
-      <button style="margin-bottom: 4px" class="btn btn-xs btn-default" type="button" data-toggle="collapse" data-target="#edit" aria-expanded="false" aria-controls="edit">
-      <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> 수정
-      </button>
-      <div class="collapse" id="edit">
-        <div class="well well-sm">
-          <form action="<?php echo $page_location;?>" method="post">
-            <textarea style="margin-bottom: 8px" class="form-control" rows="4" name="user-info" maxlength="1000"><?php echo $page_response['user']['info'];?></textarea>
-            <div class="text-center">
-              <p>
-                <button type="submit" class="btn btn-primary btn-sm">저장</button>
-                <button type="button" class="btn btn-default btn-sm" data-toggle="collapse" data-target="#edit">취소</button>
-              </p>
-            </div>
-          </form>
-        </div>
-      </div>
-      <?php }?>
-      </p>
-      <footer>계정 생성일 <cite><?php echo $page_response['user']['timestamp'];?></cite>
-      </footer>
-    </blockquote>
-    <table class="table table-hover">
+  <h5 class="mt-5">Profile</h5>
+  <span>Joined: <em><?php echo($page["user"]["timestamp"]);?></em></span><br/>
+  <form id="intro">
+    <span id="introText" class="mr-2"><?php echo($page["user"]["info"]);?></span>
+    <button type="button" class="btn btn-default btn-sm my-2" id="introButton">Edit</button>
+  </form>
+
+  <h5 class="mt-5">Contribution History</h5>
+  <table class="table table-hover">
       <thead>
         <tr>
           <th class="text-center" style="width: 10%">#</th>
-          <th class="text-center" style="width: 45%">항목</th>
-          <th class="text-center" style="width: 15%">변동</th>
-          <th class="text-center" style="width: 15%">비교</th>
-          <th class="text-center" style="width: 25%">편집 시간</th>
+          <th class="text-center" style="width: 45%">Article</th>
+          <th class="text-center" style="width: 15%">Comment</th>
+          <th class="text-center" style="width: 15%">Fluctuation</th>
+          <th class="text-center" style="width: 25%">Date</th>
         </tr>
       </thead>
+
       <tbody class="text-center">
-        <?php 
-        if (count($page_response['user']['contributions']) < 1) {
-            echo '<tr><td></td><td><em>아직 기여 기록이 없습니다.</em></td><td></td><td></td><td></td></tr>';
-        }
-
-        foreach ($page_response['user']['contributions'] as $result) {
-
+      <?php
+        if (count($page["user"]["contributions"]) < 1) {
+            echo '<tr><td colspan="6">No record</td></tr>';
+        } 
+        $next_id = 0;
+        foreach ($page["user"]["contributions"]  as $result) {
             echo '<tr>';
-            echo '<td id="c"><a href="'.HREF_REVISION.'?i='.$result["id"].'">'.$result["id"].'</a></td>';
-
-            if (strlen($result["comment"]) > 0) {
-                echo '<td id="c"><a href="'.HREF_READ.'/'.$result["article_id"].'"">'.$result["article_title"].'</a><br>('.$result["comment"].')</td>';
-            } else {
-                echo '<td id="c"><a href="'.HREF_READ.'/'.$result["article_id"].'"">'.$result["article_title"].'</a></td>';
-            }
-
+            echo '<td><a href="./?revision&i=' . $result["id"] . '">' . $result["id"] . '</a></td>';
+            echo '<td><a href="./?read&i=' . $result["article_id"] . '">' . $result["article_title"] . '</a></td>';
+            echo '<td>' . $result["comment"] . '</td>';
+            
             $fluctuation = intval($result["fluctuation"]);    
             if($fluctuation > 0) {
-                echo '<td id="c"><span style="color:green"><span class="glyphicon glyphicon-arrow-up" aria-hidden="true" ></span> '.$fluctuation.'</span>';
+                echo '<td><span style="color:green">+' . $fluctuation . '</span>';
             } else if ($fluctuation == 0) {
-                echo '<td id="c"><span style="color:grey"><span class="glyphicon glyphicon-minus" aria-hidden="true" ></span> 0</span>';
+                echo '<td><span style="color:grey">-</span>';
             } else {
-                echo '<td id="c"><span style="color:red"><span class="glyphicon glyphicon-arrow-down" aria-hidden="true" ></span> '.abs($fluctuation).'</span>';
+                echo '<td><span style="color:red">-' . abs($fluctuation) . '</span>';
             }
             echo '</td>';
-            echo '<td>
-                <a href="'.HREF_REVISIONS.'/'.$result["article_id"].'" class="btn btn-xs btn-default" type="button">역사</a> 
-                <a href="'.HREF_REVISION.'?i='.$result["id"].'&compare=1" class="btn btn-xs btn-default" type="button">비교</a></td>';
-            echo '<td id="c">'.$result["timestamp"].'</td>';
-            echo '</tr>'; 
-        }
-        ?>
-      </tbody>
-    </table>
-    <nav>
-      <ul class="pager">
-        <?php
-        if($page_response['page'] > 0) {
-            echo '<li class="previous"><a href="'.$page_location.'&p='.($page_response['page'] - 1).'"><span aria-hidden="true">&larr;</span> 최근 기여 보기</a></li>';
-        }
-        if (count($page_response['user']['contributions']) >= MAX_REVISIONS) {
-            echo '<li class="next"><a href="'.$page_location.'&p='.($page_response['page'] + 1).'">이전 기여 보기 <span aria-hidden="true">&rarr;</span></a></li>';
-        }
-        ?>
-      </ul>
-    </nav>
-  </div>
-  <br/>
-  <br/>
+            echo '<td>' . $result["timestamp"] . '</td>';
+            echo '</tr>';
+            $next_id = $result["id"];
+        }?>
+    </tbody>
+  </table>
 </div>
+<script>
 
-<?php include 'frame.footer.php';?>
+window.onload = function() {
+
+    var intro = document.getElementById("intro");
+    var introText = document.getElementById("introText");
+    var introButton = document.getElementById("introButton");
+    var introCancelButton = introButton.cloneNode(true);
+    var introInput = document.createElement("input");
+
+    var originalText = introText.textContent;
+    var editMode = false;
+
+    introInput.type = "text";
+    introInput.classList.add("form-control");
+    introCancelButton.textContent = "Cancel";
+    introCancelButton.classList.add("mx-2");
+
+    introButton.onclick = introCancelButton.onclick =  function(event) {
+        editMode = !editMode;
+        if (editMode) {
+            introInput.value = originalText;
+            intro.removeChild(introText);
+            intro.insertBefore(introInput, introButton);
+            intro.appendChild(introCancelButton, introButton);
+            introButton.textContent = "Save";
+        } else {
+            if (event.target == introCancelButton) {
+                intro.removeChild(introInput);
+                intro.removeChild(introCancelButton);
+                intro.insertBefore(introText, introButton);
+                introButton.textContent = "Edit";
+            } else {
+                // 폼 전송
+                post("./?profile", {
+                    "user-name": "<?php echo($page["user"]["name"]);?>",
+                    "user-info": introInput.value
+                });
+            }
+        }
+    }
+};
+
+function post(path, params, method) {
+    method = method || "post"; 
+
+    var form = document.createElement("form");
+    form.setAttribute("method", method);
+    form.setAttribute("action", path);
+
+    for(var key in params) {
+        if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+         }
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+}
+
+</script>
+
+
+<?php
+require_once __DIR__ . "/frame.footer.php";
+?>
