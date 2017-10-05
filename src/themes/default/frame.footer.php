a58a75bbf7fe88197e3c13fdb73a57b131b69af8
HyunJun Kim <hyunjun.leo.kim@gmail.com>
2017-10-05 23:30:15 +0900

    Fix dir

--- src/themes/default/frame.footer.php
+++ src/themes/default/frame.footer.php
@@ -4,8 +4,8 @@
         <p class="text-muted my-3"><?php echo(SettingsManager::get_instance()->get("site_description"));?></p></div>
       </div>
     </footer>
-    <script src=".<?php echo(YAONGWIKI_THEME_DIR);?>/js/jquery-3.2.1.min.js"></script>
-    <script src=".<?php echo(YAONGWIKI_THEME_DIR);?>/js/popper.min.js"></script>
-    <script src=".<?php echo(YAONGWIKI_THEME_DIR);?>/js/bootstrap.min.js"></script>
+    <script src=".<?php echo(YAONGWIKI_THEME_DIRNAME);?>/js/jquery-3.2.1.min.js"></script>
+    <script src=".<?php echo(YAONGWIKI_THEME_DIRNAME);?>/js/popper.min.js"></script>
+    <script src=".<?php echo(YAONGWIKI_THEME_DIRNAME);?>/js/bootstrap.min.js"></script>
   </body>
 </html>
