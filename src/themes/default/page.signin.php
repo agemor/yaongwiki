327e22a7a04d1dd22d9c979af5fdad6b4812ff7b
HyunJun Kim <hyunjun.leo.kim@gmail.com>
2017-10-05 23:10:41 +0900

    Indent signin

--- src/themes/default/page.signin.php
+++ src/themes/default/page.signin.php
@@ -10,36 +10,35 @@
 require_once YAONGWIKI_CORE_DIR . "/page.signin.processor.php";
 
 $page = process();
-$settings = SettingsManager::get_instance();
-$http_vars = HttpVarsManager::get_instance();
 
 if (isset($page["redirect"])) {
     redirect($page["redirect"]);
     exit();
 }
 
-$page["title"] = "Sign in" . " - " . $settings->get("site_title");
-
-require_once __DIR__ . "/frame.header.php";
+$page["title"] = "Sign in" . " - " . SettingsManager::get_instance()->get("site_title");
 ?>
+
+<?php require_once __DIR__ . "/frame.header.php"; ?>
 <div class="container">
+
   <div class="title my-4">
-    <h2>
-    Sign In
-    <h2>
+    <h2>Sign In</h2>
   </div>
+
   <?php if (isset($page["result"]) && $page["result"] !== true) { ?>
   <div class="alert alert-danger" role="alert">
     <?php echo($page["message"]);?>
   </div>
   <?php } ?>
-	<form action="./?signin&redirect=<?php echo($http_vars->get("redirect"));?>" method="post">
+
+	<form action="./?signin&redirect=<?php echo(HttpVarsManager::get_instance()->get("redirect"));?>" method="post">
 		<div class="row my-4">
 			<div class="col-md-6">
 				<p>Please enter your account name and password.</p>
 				<div class="form-group">
 					<label for="nameInput">User Name</label>
-					<input type="text" name="user-name" class="form-control" id="nameInput" value="<?php echo($http_vars->get("user-name"));?>" required>
+					<input type="text" name="user-name" class="form-control" id="nameInput" value="<?php echo(HttpVarsManager::get_instance()->get("user-name"));?>" required>
 				</div>
 				<div class="form-group">
 					<label for="passwordInput">User Password</label>
@@ -55,6 +54,4 @@ require_once __DIR__ . "/frame.header.php";
 		</div>
 	</form>
 </div>
-<?php
-require_once __DIR__ . "/frame.footer.php";
-?>
+<?php require_once __DIR__ . "/frame.footer.php"; ?>
